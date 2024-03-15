7c086e4bd5fba9972f71ea95489da540d29f32a5
rSaydelleS <rafael.ppuerari@gmail.com>
2024-03-15 19:27:20 -0300

    Testing the form hook

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -1,14 +1,21 @@
+"use client";
 
 import { useForm } from "react-hook-form";
 
 export default function Form() {
   const { register, handleSubmit } = useForm();
 
+  const onSubmit = (data) => {
+    
+    console.log(data);
+  }
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="Email"> Email address</label>
         <input name="" {...register("Email", { required: true })} />
+
+      <input type="submit" value="Subscribe to monthly newslatter"/>
       </form>
     </>
   );
